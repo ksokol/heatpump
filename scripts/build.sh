19d668cacea1c78a4c5f67c6b79dd82617a226bb
Kamill Sokol <dev@sokol-web.de>
2020-04-18 16:31:36 +0200

    Create x86 and arm binaries.

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,6 +3,11 @@ set -e
 
 export GO111MODULE=on
 
-rm -rf bin/heatpump
+rm -rf bin/heatpump*
 
-CGO_ENABLED=0 GOGC=off go build -o bin/heatpump ./cmd/heatpump
+CGO_ENABLED=0 GOGC=off go build -o bin/heatpump-x86 ./cmd/heatpump
+
+export GOARCH=arm
+export GOARM=6
+
+CGO_ENABLED=0 GOGC=off go build -o bin/heatpump-arm ./cmd/heatpump
