4b3e3908d263a5271ca4ffe960a0094769b80686
Kamill Sokol <dev@sokol-web.de>
2020-04-19 20:47:06 +0200

    Configure http client timeouts.

--- internal/sink/sink.go
+++ internal/sink/sink.go
@@ -6,9 +6,12 @@ import (
 	"heatpump/internal/cli"
 	"io"
 	"log"
+	"net"
 	"net/http"
 	"time"
 )
+
+const timeout = 1 * time.Second
 
 var url string
 var basicAuth string
@@ -18,6 +21,25 @@ func init() {
 
 	url = fmt.Sprintf("%s/write?precision=s&db=%s", cliOptions.Target, cliOptions.Db)
 	basicAuth = "Basic " + base64.StdEncoding.EncodeToString([]byte(cliOptions.Username+":"+cliOptions.Password))
+}
+
+func newHttpClient() *http.Client {
+	return &http.Client{
+		Transport: &http.Transport{
+			DialContext: (&net.Dialer{
+				Timeout:   timeout,
+				KeepAlive: timeout,
+			}).DialContext,
+			ForceAttemptHTTP2:     false,
+			MaxIdleConns:          1,
+			IdleConnTimeout:       timeout,
+			TLSHandshakeTimeout:   timeout,
+			ExpectContinueTimeout: timeout,
+			DisableKeepAlives:     true,
+			MaxConnsPerHost:       1,
+		},
+		Timeout: timeout,
+	}
 }
 
 func SendMetric(reader io.Reader) error {
@@ -31,15 +53,10 @@ func SendMetric(reader io.Reader) error {
 
 	req.Header.Add("Authorization", basicAuth)
 
-	client := &http.Client{
-		Timeout: 1 * time.Second,
-	}
+	client := newHttpClient()
+	defer client.CloseIdleConnections()
 
 	resp, err := client.Do(req)
-
-	if err != nil {
-		client.CloseIdleConnections()
-	}
 
 	if err == nil && resp.StatusCode != 204 {
 		err = fmt.Errorf("%v", resp)
